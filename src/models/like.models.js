9afa0509d1cf7ff3c6788e78b4b8fe1da11434cb
Virendra-Balasaheb-Desai <desaivirendra959@gmail.com>
2026-01-14 11:48:48 +0530

    playlist, comment, like and tweet model

---
+++ src/models/like.models.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+const likeSchema = new mongoose.Schema(
+    {
+        likeBy: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User"
+        },
+        video: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Video"
+        },
+        comment: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Comment"
+        },
+        tweet: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Tweet"
+        }
+    },
+    {
+        timestamps: true
+    }
+)
+
+export const Like = mongoose.model("Like", likeSchema)
